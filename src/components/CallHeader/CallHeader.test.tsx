3bce678d6931a17334744a4d6b52b5b1603de4ed
Miguel <miguee02@gmail.com>
2022-11-18 12:59:36 +0100

    Added setup for tests, to be able to use Tractor while rendering any component with UI. Added tests for CallHeader component

---
+++ src/components/CallHeader/CallHeader.test.tsx
@@ -0,0 +1,47 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { test } from '@jest/globals';
+import { screen } from '@testing-library/react';
+import CallHeader from '../CallHeader';
+import {
+    MockedInboundCall,
+    MockedOutboundCall,
+    render,
+} from '../../test-utils';
+
+test('Render outbound Call CallHeader with "to" in it', async () => {
+    render(<CallHeader call={MockedOutboundCall} />);
+
+    screen.getByText(MockedOutboundCall.to);
+});
+
+test('Render inbound Call CallHeader with "from" in it', async () => {
+    render(<CallHeader call={MockedInboundCall} />);
+
+    screen.getByText(MockedInboundCall.from);
+});
+
+test('Render call without buttons', async () => {
+    render(<CallHeader call={MockedInboundCall} />);
+
+    expect(screen.queryByText('Detail')).toBeNull();
+    expect(screen.queryByText('Archive')).toBeNull();
+});
+
+test('Render call with Detail button', async () => {
+    render(
+        <CallHeader call={MockedInboundCall} goToDetail={(_: string) => {}} />
+    );
+
+    expect(screen.getByText('Detail')).not.toBeNull();
+    expect(screen.queryByText('Archive')).toBeNull();
+});
+
+test('Render call with Archive button', async () => {
+    render(<CallHeader call={MockedInboundCall} archive={(_: string) => {}} />);
+
+    expect(screen.getByText('Archive')).not.toBeNull();
+    expect(screen.queryByText('Detail')).toBeNull();
+});
---
+++ src/test-utils.tsx
@@ -0,0 +1,42 @@
+import { render, RenderOptions } from '@testing-library/react';
+import { Tractor } from '@aircall/tractor';
+import { Call } from './api/types';
+import { FC, ReactElement } from 'react';
+
+export const MockedOutboundCall: Call = {
+    id: '123',
+    duration: 59566,
+    is_archived: false,
+    from: '+33171451698',
+    to: '+33128210740',
+    direction: 'outbound',
+    call_type: 'missed',
+    via: '+33120801106',
+    created_at: '2022-11-18T06:52:17.368Z',
+    notes: [],
+};
+
+export const MockedInboundCall: Call = {
+    id: '123',
+    duration: 59566,
+    is_archived: false,
+    from: '+33171451698',
+    to: '+33128210740',
+    direction: 'inbound',
+    call_type: 'missed',
+    via: '+33120801106',
+    created_at: '2022-11-18T06:52:17.368Z',
+    notes: [],
+};
+
+const AllTheProviders: FC<{ children: React.ReactNode }> = ({ children }) => {
+    return <Tractor injectStyle>{children}</Tractor>;
+};
+
+const customRender = (
+    ui: ReactElement,
+    options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from '@testing-library/react';
+export { customRender as render };
